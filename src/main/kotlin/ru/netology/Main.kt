2c01a5f983c7801bce4f7e94eada746ec0b30ce1
vanya <whitebeltvanya@gmail.com>
2025-08-13 18:05:00 +0300

    Changed procent calculation

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -1,13 +1,13 @@
 package ru.netology
 
-const val FEE_RATE = 0.075
+const val FEE_PROCENT = 0.75
 const val MIN_FEE = 35.00
 
 fun main() {
     var transferFee: Double
     val amounts = listOf(10, 100, 200, 300, 500, 1000, 10_000, 10_000_000)
     for (amount in amounts) {
-        transferFee = amount * FEE_RATE
+        transferFee = (amount * FEE_PROCENT)/100
         transferFee = if (transferFee > MIN_FEE) transferFee else MIN_FEE
         println("Размер комиссии равен: $transferFee для суммы $amount")
     }
